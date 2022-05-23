05959106e0cfaebc7a4e22cf34a7dab59a2311af
Ghostsvd <93258974+Ghostsvd@users.noreply.github.com>
2022-05-23 07:52:07 +0300

    Добавьте файлы проекта.

---
+++ app_16/FriendPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app_16
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FriendPage : ContentPage
+    {
+        public Friend Model { get; private set; }
+        public ApplicationvVewModel ViewModel { get; private set; }
+        public FriendPage(Friend model, ApplicationvVewModel viewModel)
+        {
+            InitializeComponent();
+            Model = model;
+            ViewModel = viewModel;
+            this.BindingContext = this;
+        }
+    }
+}
---
+++ app_16/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace app_16
+{
+    public partial class MainPage : ContentPage
+    {
+        ApplicationvVewModel viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new ApplicationvVewModel() { Navigation = this.Navigation };
+            BindingContext = viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            await viewModel.GetFriends();
+            base.OnAppearing();
+        }
+    }
+}
